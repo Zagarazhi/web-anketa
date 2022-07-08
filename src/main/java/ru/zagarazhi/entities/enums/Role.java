e3065aa769b4b79d88163df8459ff71da048f4bc
Zagarazhi <fukuroOwl@yandex.ru>
2022-07-09 01:17:39 +0400

    update roles names

--- src/main/java/ru/zagarazhi/entities/enums/Role.java
+++ src/main/java/ru/zagarazhi/entities/enums/Role.java
@@ -3,7 +3,7 @@ package ru.zagarazhi.entities.enums;
 import org.springframework.security.core.GrantedAuthority;
 
 public enum Role implements GrantedAuthority{
-    USER, ADMIN;
+    ROLE_USER, ROLE_ADMIN;
 
     @Override
     public String getAuthority(){
--- src/main/java/ru/zagarazhi/services/impl/UserServiceImpl.java
+++ src/main/java/ru/zagarazhi/services/impl/UserServiceImpl.java
@@ -97,7 +97,7 @@ public class UserServiceImpl implements UserService {
         user.setUsername(registration.getUsername());
         user.setEmail(registration.getEmail());
         user.setRating((long) 0);
-        user.setRoles(new HashSet<>(Arrays.asList(Role.USER, Role.ADMIN)));
+        user.setRoles(new HashSet<>(Arrays.asList(Role.ROLE_USER, Role.ROLE_ADMIN)));
         user.setPassword(passwordEncoder().encode(registration.getPassword()));
         String randomCode = RandomString.make(64);
         user.setVerificationCode(randomCode);
